cdf5867b3356f729268001821836416be03ec912
OuardiFatiha <ouardiifatiha@gmail.com>
2022-11-11 18:40:13 +0100

    adding organizing_containers_of_balls

---
+++ organizing_containers_of_balls.cpp
@@ -0,0 +1,122 @@
+// link = https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=60-day-campaign&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'organizingContainers' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts 2D_INTEGER_ARRAY container as parameter.
+ */
+
+string organizingContainers(vector<vector<int>> container) {
+    int n = container.size();
+    int m = container[0].size();
+    vector<int> colSum;
+    vector<int> rowSum;
+    int sum;
+    
+    for(int i=0; i<n; i++){
+        sum = 0;
+        for(int j=0; j<m; j++){
+            sum += container[i][j];
+        }
+        colSum.push_back(sum);
+    }
+    
+    for(int j=0; j<m; j++){
+        sum = 0;
+        for(int i=0; i<n; i++){
+            sum += container[i][j];
+        }
+        rowSum.push_back(sum);
+    }
+    
+    sort(colSum.begin(), colSum.end());
+    sort(rowSum.begin(), rowSum.end());
+    if(colSum == rowSum) return "Possible";
+    else return "Impossible";
+}
+
+int main()
+{
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<vector<int>> container(n);
+
+        for (int i = 0; i < n; i++) {
+            container[i].resize(n);
+
+            string container_row_temp_temp;
+            getline(cin, container_row_temp_temp);
+
+            vector<string> container_row_temp = split(rtrim(container_row_temp_temp));
+
+            for (int j = 0; j < n; j++) {
+                int container_row_item = stoi(container_row_temp[j]);
+
+                container[i][j] = container_row_item;
+            }
+        }
+
+        string result = organizingContainers(container);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
